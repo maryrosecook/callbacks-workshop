97298bb9e265574bf65e569fd8cb1f22a3b42f82
Mary Rose Cook <mary@maryrosecook.com>
2016-08-10 10:47:54 +0100

    Remove old question code and log `hello` in the starter JS file

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-$.get("https://async-workshops-api.herokuapp.com/people", function(peopleResponse) {
-  peopleResponse.forEach(function(person) {
-    $.get("https://async-workshops-api.herokuapp.com/people/" + person.id, function(personResponse) {
-      console.log(personResponse.favouriteMusic);
-    });
-  });
-});
+console.log("hello!");
